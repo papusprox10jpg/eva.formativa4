77a0781756b03840fc8e1d9637a96bf05f17ed4e
nearQurita <go.ulloa@duocuc.cl>
2022-06-03 15:28:48 -0400

    Arregle telefono, prints y nombre matriz

--- eva.formativa4.py
+++ eva.formativa4.py
@@ -23,18 +23,19 @@ while menu:
     except ValueError:
       print("\nError, Ingrese la opcion como un numero")
   if op == 1:
-    matriz = [["|"," 1"," 2"," 3","     "," 4"," 5"," 6","|"],["|"," 7"," 8"," 9","     ","10","11","12","|"],
+    print("\nAsientos del avión: ")
+    avion = [["|"," 1"," 2"," 3","     "," 4"," 5"," 6","|"],["|"," 7"," 8"," 9","     ","10","11","12","|"],
               ["|","13","14","15","    "," 16","17","18","|"],["|","19","20","21","     ","22","23","24","|"],
               ["|","25","26","27","    "," 28","29","30","|"],["|―","――","――","―","     ","―","――","――","―|"],
               ["|_","__","__","_","     ","_","__","__","_|"],["|","31","32","33","    "," 34","35","36","|"],
               ["|","37","38","39","    "," 40","41","42","|"]]
     for i in range(9):
       for j in range(9):
-          print(matriz[i][j],end=" ")
+          print(avion[i][j],end=" ")
       print()
     print("\nAsientos ocupados: ")
-    print(dispo(matriz))
-    print("\n1. Asientos normales \n2. Asientos VIP\n")
+    print(dispo(avion))
+    print("\n1. Asientos normales: del 1 al 30 \n2. Asientos VIP: del 31 al 42\n")
 
   elif op == 2:
     nom = str("\nIngrese su nombre: \t")
@@ -46,26 +47,26 @@ while menu:
         else:
           print("\nError, Rut no valido, ingrese correctamente ")
       except ValueError:
-        print("\nError, ingrese su rut sin puntos ni digito verificador")
+        print("\nError, ingrese su rut sin puntos ni digito verificador")   
     while validacion:
       try:
-        telefono = (input("\nIngrese su numero de telefono: (comenzar con el numero 9)\t"))
-        if len(telefono) == 9:
-          break
-        else :
+        telefono = int(input("\nIngrese su numero de telefono: (comenzar con el numero 9)\t"))
+        if len(str(telefono)) == 9:
+          for i in str(telefono):
+            if "9" in i:
+              break
+            else:
+              cont += 1
+          if cont >= 1:
+            print("\nError, Su Telefono debe tener un 9 al principio")
+            cont = 0
+          else:
+            break
+        else:
           print("\nError, su numero debe tener 9 digitos")
       except ValueError:
-        print("\nError, ingrese su telefono como numero ")
-        
-      for i in telefono:
-        if "9" in i:
-          break
-        else:
-          cont += 1
-      if cont >= 1:
-        print("\nError, Su Telefono debe tener un 9 al principio")
-        cont = 0
-  
+        print("\nError, ingrese su telefono como numeros")
+
   elif op == 3:
     print("op3") #momentaneo!
   elif op == 4:
