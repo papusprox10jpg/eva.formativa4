c4e8f242229dd1054093114a35d215633a0fa859
Gabosaurio-rex <101837552+Gab-Serrano@users.noreply.github.com>
2022-06-04 01:16:01 -0400

    Agrega doc de funcs, agrega funcs

--- eva.formativa4.py
+++ eva.formativa4.py
@@ -2,48 +2,43 @@ import funciones_avion as fun
 validacion = True
 menu = True 
 cont = 0
+avion = [["|"," 1"," 2"," 3","     "," 4"," 5"," 6","|"],["|"," 7"," 8"," 9","     ","10","11","12","|"],
+        ["|","13","14","15","    "," 16","17","18","|"],["|","19","20","21","     ","22","23","24","|"],
+        ["|","25","26","27","    "," 28","29","30","|"],["|―","――","――","―","     ","―","――","――","―|"],
+        ["|_","__","__","_","     ","_","__","__","_|"],["|","31","32","33","    "," 34","35","36","|"],
+        ["|","37","38","39","    "," 40","41","42","|"]] #Gab/sugerencia: Muevo asiento a arriba para organizar las funciones.
 while menu:
-  print("\nVuelos-Duoc")
-  print("\nBienvenido!")
-  print("\n1. Ver asientos disponibles \n2. Comprar asientos \n3. Anular vuelo \n4. modificar datos de pasajero \n5. Salir \n ")
+  print("\n¡Bienvenido a Vuelos-Duoc!")
+  print("\n1. Ver asientos disponibles. \n2. Comprar asientos. \n3. Anular vuelo. \n4. Modificar datos de pasajero. \n5. Salir. \n ")
   while validacion:
     try:
-      op = int(input("\nIngrese la opcion que desee: \t"))
-      if op == 1 or op == 2 or op == 3 or op == 4 or op == 5:
+      op = int(input("\nIngrese la opción que desee: \t"))
+      if op >=1 and op<=5:
         break
       else:
-        print("\nError, Ingrese un numero entre el 1 y 5")
+        print("\nError: Ingrese un numero entre el 1 y 5")
     except ValueError:
-      print("\nError, Ingrese la opcion como un numero")
+      print("\nError: Ingrese la opcion como un numero")
   if op == 1:
-    print("\nAsientos del avión: ")
-    avion = [["|"," 1"," 2"," 3","     "," 4"," 5"," 6","|"],["|"," 7"," 8"," 9","     ","10","11","12","|"],
-              ["|","13","14","15","    "," 16","17","18","|"],["|","19","20","21","     ","22","23","24","|"],
-              ["|","25","26","27","    "," 28","29","30","|"],["|―","――","――","―","     ","―","――","――","―|"],
-              ["|_","__","__","_","     ","_","__","__","_|"],["|","31","32","33","    "," 34","35","36","|"],
-              ["|","37","38","39","    "," 40","41","42","|"]]
-    for i in range(9):
-      for j in range(9):
-          print(avion[i][j],end=" ")
-      print()
-    print("\nAsientos ocupados: ")
-    print(disp_asiento(avion))
-    print("\n1. Asientos normales: del 1 al 30 \n2. Asientos VIP: del 31 al 42\n")
-
+    fun.clear()
+    fun.disp_asiento(avion)
+    fun.cant_ocupado(avion)
+    fun.ir_menu()
   elif op == 2:
+    fun.clear()
     nom = str("\nIngrese su nombre: \t")
     while validacion:
       try:
-        rut = int(input("\nIngrese su rut: \t"))
+        rut = int(input("\nIngrese su RUT: \t"))
         if rut >= 5000000 and rut <= 99999999:
           break
         else:
-          print("\nError, Rut no valido, ingrese correctamente ")
+          print("\nError: RUT inválido, ingrese correctamente ")
       except ValueError:
-        print("\nError, ingrese su rut sin puntos ni digito verificador")   
+        print("\nError: ingrese su RUT sin puntos ni digito verificador")   
     while validacion:
       try:
-        telefono = int(input("\nIngrese su numero de telefono: (comenzar con el numero 9)\t"))
+        telefono = int(input("\nIngrese su número de telefono: (comenzar con el numero 9)\t"))
         if len(str(telefono)) == 9:
           for i in str(telefono):
             if "9" in i:
@@ -51,19 +46,23 @@ while menu:
             else:
               cont += 1
           if cont >= 1:
-            print("\nError, Su Telefono debe tener un 9 al principio")
+            print("\nError: su número de teléfono debe iniciar con un 9.")
             cont = 0
           else:
             break
         else:
-          print("\nError, su numero debe tener 9 digitos")
+          print("\nError: su número de teléfono debe tener 9 digitos")
       except ValueError:
-        print("\nError, ingrese su telefono como numeros")
-
+        print("\nError: ingrese número de teléfono como numeros")
+    fun.ir_menu()
   elif op == 3:
+    fun.clear()
     print("op3") #momentaneo!
+    fun.ir_menu()
   elif op == 4:
+    fun.clear()
     print("op4") #momentaneo!
+    fun.ir_menu()
   elif op == 5:
     print("\nSaliendo del programa...")
     break
