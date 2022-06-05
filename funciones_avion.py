3261459dc308fa3bf773d059fe340953ac81e59b
Wolfmatee <da.mallma@duocuc.cl>
2022-06-05 19:10:04 -0400

    Codigo fuente

--- funciones_avion.py
+++ funciones_avion.py
@@ -32,3 +32,50 @@ def cant_ocupado (mapa_avion):
         cant_disp+=1
   print("Asientos ocupados:", cant_disp)
   print()
+
+#Funcion compra
+def compra(compra_asiento,banco):
+  if compra_asiento > 0 and compra_asiento <= 30 :
+    normal = 0
+    normal += 78900
+    if banco == "si":
+      descuento = normal * 0.15
+      normal = normal - descuento
+      print("\n")
+      print("*"*40)
+      print("Boleta:")
+      print("* "," asientos normales\t","$78.900" )
+      print("* ","descuento 15%\t","$11.835")
+      print("*"*40)
+      print("                           su total es:\t", round(normal))
+
+    else:
+      print("\n")
+      print("*"*40)
+      print("Boleta:")
+      print("* "," asientos normales\t","$78.900" )
+      print("* ","descuento 15%\t","$0")
+      print("*"*40)
+      print("                           su total es:\t", round(normal))
+
+  if compra_asiento >= 31 and compra_asiento <= 42:
+    vip = 0
+    vip += 240000
+    if banco == "si":
+      descuento = vip * 0.15
+      vip = vip - descuento
+      print("\n")
+      print("*"*40)
+      print("Boleta:")
+      print("* "," asientos normales\t","$240.000" )
+      print("* ","descuento 15%\t","$36.000")
+      print("*"*40)
+      print("                           su total es:\t", round(vip))
+    else:
+      print("\n")
+      print("*"*40)
+      print("Boleta:")
+      print("* "," asientos normales\t","$240.000" )
+      print("* ","descuento 15%\t","$0")
+      print("*"*40)
+      print("                           su total es:\t", round(vip))
