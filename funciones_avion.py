8502262b9b614d47d9ae6b6acfbeb5e4b6588406
Wolfmatee <da.mallma@duocuc.cl>
2022-06-05 22:33:55 -0400

    cambious

--- funciones_avion.py
+++ funciones_avion.py
@@ -33,30 +33,6 @@ def cant_ocupado (mapa_avion):
   print("Asientos ocupados:", cant_disp)
   print()
 
-#Funcion telefono 
-validacion = True
-def validar_telefono(telefono):
-  cont = 0
-  while validacion:
-    try:
-      telefono = int(input("\nIngrese su numero de telefono: (comenzar con el numero 9)\t"))
-      if len(str(telefono)) == 9:
-        for i in str(telefono):
-          if "9" in i:
-            return telefono
-            break
-          else:
-            cont += 1
-        if cont >= 1:
-          print("\nError, Su Telefono debe tener un 9 al principio")
-          cont = 0
-        else:
-          break
-      else:
-        print("\nError, su numero debe tener 9 digitos")
-    except ValueError:
-      print("\nError, ingrese su telefono como numeros")
-
 #Funcion compra
 def compra(compra_asiento,banco):
   if compra_asiento > 0 and compra_asiento <= 30 :
@@ -91,7 +67,7 @@ def compra(compra_asiento,banco):
       print("\n")
       print("*"*40)
       print("Boleta:")
-      print("* "," asientos VIP\t","$240.000" )
+      print("* "," asientos normales\t","$240.000" )
       print("* ","descuento 15%\t","$36.000")
       print("*"*40)
       print("                           su total es:\t", round(vip))
@@ -99,7 +75,18 @@ def compra(compra_asiento,banco):
       print("\n")
       print("*"*40)
       print("Boleta:")
-      print("* "," asientos VIP\t","$240.000" )
+      print("* "," asientos normales\t","$240.000" )
       print("* ","descuento 15%\t","$0")
       print("*"*40)
       print("                           su total es:\t", round(vip))
+
+#Funcion modificar
+def modificar_datos(opcion):
+  if opcion == 1:
+    print("\nModificando nombre: ")
+    nom = str(input("\nIngrese su nuevo nombre: \t"))
+    return nom
+  elif opcion == 2:
+    print("\nModificando telefono: ")
+    telefono = validar_telefono()
+    return telefono
